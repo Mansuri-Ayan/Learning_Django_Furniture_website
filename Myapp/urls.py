1a289241861e0d284573494c6ef3c4488e7aa4c2
Mansuri Ayan <2004ayanmansuri@gmail.com>
2025-09-25 09:40:20 +0530

    MyProject

---
+++ Myapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("blog/", blog, name="blog"),   
+    path("cart/", cart, name="cart"),
+    path("about/", about, name="about"),
+    path("services/", services, name="services"),
+    path("contact/", contact, name="contact"),
+    path("checkout/", checkout, name="checkout"),
+    path("shop/", shop, name="shop"),
+    path("thankyou/", thankyou, name="thankyou"),
+    path("pricing/", pricing, name="pricing"),  
+]
